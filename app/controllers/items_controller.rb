334b2d5cd66ba2072062c35b3f623136a98fcbe6
Briana <briana_brewster4@yahoo.com>
2022-11-03 10:20:53 -0500

    commit tree 94681597754e342af8c45c1acf2d78a8aa577cd0

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_error_message
 
   def index
-    items = Item.all
+    if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+    else
+      items = Item.all
+    end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item, include: :user
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_error_message
+    render json: {error: "item not found"}, status: :not_found
   end
 
 end
